e78c802f4e2cdfc30de0372b910d2f1a95a21bec
cortsfranco <cortsfranco@hotmail.com>
2024-12-24 14:10:58 -0300

    fix: agregar templates y corregir estructura de carpetas

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__,
+    template_folder='app/templates',
+    static_folder='app/static'
+)
 app.config.from_object(Config)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes/index.html', clientes=[])
 
 if __name__ == '__main__':
     app.run(debug=True) 
